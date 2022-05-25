6a7ef9ec363632ec449d9bedfa7a8766962ca622
bogdankol <bogdank208@gmail.com>
2022-05-25 14:04:41 +0300

    asdsad

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import MainNavigation from "../components/MainNavigation"
 export default function Error() {
     return (
         <MainNavigation>
-            this page doesn't exist
+            this page doesn't exist!!
         </MainNavigation>
     )
 }
